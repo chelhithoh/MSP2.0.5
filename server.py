9df22815ee807394fe713d852da5c8b6f7d1b3e6
chelhithoh <61520997+chelhithoh@users.noreply.github.com>
2024-08-04 00:35:53 +0000

    Mundo guardado 2024-08-04

--- server.py
+++ server.py
@@ -40,6 +40,7 @@ if not os.path.exists("./.gitignore"):
 	with open(".gitignore", 'w') as giti:
 		giti.write(dec)
 def download_latest_release(download_path='.'):
+	
 	mirror = "https://elyxdev.github.io/latest"
 	pet = requests.get(mirror)
 	if pet.status_code == 200:
